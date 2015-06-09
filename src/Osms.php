9688380de9baadffbcd42387d144962c52d9ab88
Ismael Toe <ismael.toe@gmail.com>
2015-06-09 17:23:53 +0000

    Converted to PSR-2 coding style guide

--- src/Osms.php
+++ src/Osms.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
 namespace Osms;
 
-class Osms 
+class Osms
 {
-	const BASE_URL = 'https://api.orange.com';
-
-	/**
-	 * Client Identifier. Unique ID provided by the Orange backend server to identify 
-	 * your application.
-	 *
-	 * @var string
-	 */
+    const BASE_URL = 'https://api.orange.com';
+    
+    /**
+     * Client Identifier. Unique ID provided by the Orange backend server to identify 
+     * your application.
+     *
+     * @var string
+     */
     protected $clientId = '';
 
     /**
@@ -35,19 +35,24 @@ class Osms
     protected $verifyPeerSSL = true;
 
     /**
-	 * Create a new Osms instance. If the user doesn't know his token or doesn't have a token 
-	 * yet, he can leave $token empty and retrieve a token with getTokenFromConsumerKey() 
-	 * method later.
-	 *
-	 * @param  string  $clientId
-	 * @param  string  $clientSecret
-	 * @param  string  $token
-	 * @param  bool    $verifyPeerSSL  Set to FALSE to stop cURL from verifying the peer's 
-	 *                                 certificate if SSL error
-	 * @return void
-	 */
-    public function __construct($clientId, $clientSecret, $token = '', $verifyPeerSSL = true)
-    {
+     * Create a new Osms instance. If the user doesn't know his token or doesn't have a
+     * token yet, he can leave $token empty and retrieve a token with
+     * getTokenFromConsumerKey() method later.
+     *
+     * @param  string  $clientId
+     * @param  string  $clientSecret
+     * @param  string  $token
+     * @param  bool    $verifyPeerSSL  Set to FALSE to stop cURL from verifying the
+     *                                 peer's certificate if SSL error
+     *
+     * @return void
+     */
+    public function __construct(
+        $clientId,
+        $clientSecret,
+        $token = '',
+        $verifyPeerSSL = true
+    ) {
         $this->clientId = $clientId;
         $this->clientSecret = $clientSecret;
         $this->token = $token;
@@ -55,310 +60,289 @@ class Osms
     }
 
     /**
-	 * Retrieves a token that will be used for all further API calls.
-	 *
-	 * @return array
-	 */
-    public function getTokenFromConsumerKey() 
-    {	
-    	$url = self::BASE_URL . '/oauth/v2/token';
+     * Retrieves a token that will be used for all further API calls.
+     *
+     * @return array
+     */
+    public function getTokenFromConsumerKey()
+    {   
+        $url = self::BASE_URL . '/oauth/v2/token';
 
-    	$credentials = $this->getClientId() . ':' . $this->getClientSecret();
+        $credentials = $this->getClientId() . ':' . $this->getClientSecret();
 
-    	$headers = array(
-    		'Authorization: Basic ' . base64_encode($credentials)
-    	);
+        $headers = array('Authorization: Basic ' . base64_encode($credentials));
 
-	    $args = array(
-	        'grant_type' => 'client_credentials',
-	    );
+        $args = array('grant_type' => 'client_credentials');
 
-	    $response = $this->callApi($headers, $args, $url, 'POST', 200);
+        $response = $this->callApi($headers, $args, $url, 'POST', 200);
 
-	    if (!empty($response['access_token']))
-	    {
-	    	$this->setToken($response['access_token']);
-	    }
+        if (!empty($response['access_token'])) {
+            $this->setToken($response['access_token']);
+        }
 
-	    return $response;
+        return $response;
     }
 
     /**
-	 * Send SMS.
-	 *
-	 * @param  string  $senderAddress    The receiver address in this format: "tel:+22500000000" 
-	 * @param  string  $receiverAddress  The receiver address in this format: "tel:+22500000000"
-	 * @param  string  $message          The content of SMS, must not exceed 160 characters         
-	 * @param  string  $senderName       The sender name
-	 * @return array
-	 */
-    public function sendSms($senderAddress, $receiverAddress, $message, $senderName = '') 
-    {	
+     * Send SMS.
+     *
+     * @param  string  $senderAddress    The receiver address in this format:
+     *                                   "tel:+22500000000"
+     * @param  string  $receiverAddress  The receiver address in this format:
+     *                                   "tel:+22500000000"
+     * @param  string  $message          The content of the SMS, must not exceed
+     *                                   160 characters         
+     * @param  string  $senderName       The sender name
+     *
+     * @return array
+     */
+    public function sendSms(
+        $senderAddress,
+        $receiverAddress,
+        $message,
+        $senderName = ''
+    ) {
+        $url = self::BASE_URL . '/smsmessaging/v1/outbound/' . urlencode($senderAddress)
+            . '/requests';
 
-    	$url = self::BASE_URL . '/smsmessaging/v1/outbound/' . urlencode($senderAddress) . '/requests';
+        $headers = array(
+            'Authorization: Bearer ' . $this->getToken(),
+            'Content-Type: application/json'
+        );
 
-    	$headers = array(
-	    	'Authorization: Bearer ' . $this->getToken(),
-	        'Content-Type: application/json'
-    	);
+        if (!empty($senderName)) {
+            $args = array(
+                'outboundSMSMessageRequest' => array(
+                    'address'                   => $receiverAddress,
+                    'senderAddress'             => $senderAddress,
+                    'senderName'                => urlencode($senderName),
+                    'outboundSMSTextMessage'    => array(
+                        'message' => $message
+                    )
+                )
+            );
+        } else {
+            $args = array(
+                'outboundSMSMessageRequest' => array(
+                    'address'                   => $receiverAddress,
+                    'senderAddress'             => $senderAddress,
+                    'outboundSMSTextMessage'    => array(
+                        'message' => $message
+                    )
+                )
+            );
+        }
 
-    	if (!empty($senderName))
-    	{
-		    $args = array(
-		        'outboundSMSMessageRequest' => array(
-		            'address'                   => $receiverAddress,
-		            'senderAddress'             => $senderAddress,
-		            'senderName'                => urlencode($senderName),
-		            'outboundSMSTextMessage'    => array(
-		                'message' => $message
-		            )
-		        )
-	    	);
-	    }
-	    else
-	    {
-	    	$args = array(
-		        'outboundSMSMessageRequest' => array(
-		            'address'                   => $receiverAddress,
-		            'senderAddress'             => $senderAddress,
-		            'outboundSMSTextMessage'    => array(
-		                'message' => $message
-		            )
-		        )
-	    	);
-	    }
-
-	    return $this->callApi($headers, $args, $url, 'POST', 201, true);
+        return $this->callApi($headers, $args, $url, 'POST', 201, true);
     }
 
     /**
-	 * Lists SMS usage statistics per application.
-	 *
-	 * @param  array  $args  An associative array to filter the results, 
-	 *                       containing country (the international 3 digits country code) 
-	 *                       and/or appid (you can retrieve the application ID from your 
-	 *                       dashboard application)
-	 * @return array
-	 */
-    public function getAdminStats($args = null) 
-    {	
-    	$url = self::BASE_URL . '/sms/admin/v1/statistics';
+     * Lists SMS usage statistics per application.
+     *
+     * @param  array  $args  An associative array to filter the results, containing
+     *                       country (the international 3 digits country code) and/or
+     *                       appid (you can retrieve your application ID from your 
+     *                       dashboard application)
+     *
+     * @return array
+     */
+    public function getAdminStats($args = null)
+    {   
+        $url = self::BASE_URL . '/sms/admin/v1/statistics';
 
-    	$headers = array(
-    		'Authorization: Bearer ' . $this->getToken()
-    	);
+        $headers = array('Authorization: Bearer ' . $this->getToken());
 
-	    return $this->callApi($headers, $args, $url, 'GET', 200);
+        return $this->callApi($headers, $args, $url, 'GET', 200);
     }
 
     /**
-	 * Displays how many SMS you can still send.
-	 *
-	 * @param  string  $country  The country to filter on (the international 3 digits country)
-	 * @return array
-	 */
-    public function getAdminContracts($country = '') 
+     * Displays how many SMS you can still send.
+     *
+     * @param  string  $country  The country to filter on (the international 3 digits 
+     *                           country)
+     *
+     * @return array
+     */
+    public function getAdminContracts($country = '')
     {
-    	$url = self::BASE_URL . '/sms/admin/v1/contracts';
+        $url = self::BASE_URL . '/sms/admin/v1/contracts';
 
-    	$headers = array(
-    		'Authorization: Bearer ' . $this->getToken()
-    	);
+        $headers = array('Authorization: Bearer ' . $this->getToken());
 
-    	$args = null;
+        $args = null;
 
-    	if (!empty($country))
-    	{
-		    $args = array(
-		        'country' => $country,
-		    );
-		}
+        if (!empty($country)) {
+            $args = array('country' => $country);
+        }
 
-	    return $this->callApi($headers, $args, $url, 'GET', 200);
+        return $this->callApi($headers, $args, $url, 'GET', 200);
     }
 
     /**
-	 *  Lists your purchase history.
-	 *
-	 * @param  string  $country  The country to filter on (the international 3 digits country)
-	 * @return array
-	 */
+     *  Lists your purchase history.
+     *
+     * @param  string  $country  The country to filter on (the international 3 digits
+     *                           country)
+     *
+     * @return array
+     */
     public function getAdminPurchasedBundles($country = '')
     {
-    	$url = self::BASE_URL . '/sms/admin/v1/purchaseorders';
+        $url = self::BASE_URL . '/sms/admin/v1/purchaseorders';
 
-    	$headers = array(
-    		'Authorization: Bearer ' . $this->getToken()
-    	);
+        $headers = array('Authorization: Bearer ' . $this->getToken());
 
-    	$args = null;
+        $args = null;
 
-    	if (!empty($country))
-    	{
-		    $args = array(
-		        'country' => $country,
-		    );
-		}
+        if (!empty($country)) {
+            $args = array('country' => $country);
+        }
 
-	    return $this->callApi($headers, $args, $url, 'GET', 200);
+        return $this->callApi($headers, $args, $url, 'GET', 200);
     }
 
     /**
-	 *  Get the Cliend ID.
-	 *
-	 * @return string
-	 */
+     *  Get the Cliend ID.
+     *
+     * @return string
+     */
     public function getClientId()
     {
-    	return $this->clientId;
+        return $this->clientId;
     }
 
     /**
-	 *  Call API Endpoints.
-	 *
-	 * @param  array   $headers         An array of HTTP header fields to set
-	 * @param  array   $args            The data to send
-	 * @param  string  $url             The URL to fetch
-	 * @param  string  $method          Whether to do a HTTP POST or a HTTP GET
-	 * @param  int     $successCode     The HTTP code that will be returned on success
-	 * @param  bool    $jsonEncodeArgs  Whether or not to json_encode $args
-	 * @return array   Contains the results returned by the endpoint or an error message 
-	 */
-    public function callApi($headers, $args, $url, $method, $successCode, $jsonEncodeArgs = false)
-    {
-    	$ch = curl_init();
- 	
- 		if ($method === 'POST')
- 		{
-	    	curl_setopt($ch, CURLOPT_URL, $url);
-	 		curl_setopt($ch, CURLOPT_POST, true);
+     *  Call API Endpoints.
+     *
+     * @param  array   $headers         An array of HTTP header fields to set
+     * @param  array   $args            The data to send
+     * @param  string  $url             The URL to fetch
+     * @param  string  $method          Whether to do a HTTP POST or a HTTP GET
+     * @param  int     $successCode     The HTTP code that will be returned on
+     *                                  success
+     * @param  bool    $jsonEncodeArgs  Whether or not to json_encode $args
+     *
+     * @return array   Contains the results returned by the endpoint or an error
+     *                 message
+     */
+    public function callApi(
+        $headers,
+        $args,
+        $url,
+        $method,
+        $successCode,
+        $jsonEncodeArgs = false
+    ) {
+        $ch = curl_init();
+    
+        if ($method === 'POST') {
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, true);
 
-	 		if (!empty($args))
-	 		{
-		 		if ($jsonEncodeArgs === true)
-		 		{
-		 			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
-		 		}
-		 		else
-		 		{
-		 			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($args));
-		 		}
-	 		}
-	 	} // GET
-	 	else
-	 	{	
-	 		if (!empty($args))
-	 		{
-	 			curl_setopt($ch, CURLOPT_URL, $url . '?' . http_build_query($args));
-	 		}
-	 		else
-	 		{
-	 			curl_setopt($ch, CURLOPT_URL, $url);
-	 		}
-	 	}
+            if (!empty($args)) {
+                if ($jsonEncodeArgs === true) {
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
+                } else {
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($args));
+                }
+            }
+        } else /* $method === 'GET' */ {
+            if (!empty($args)) {
+                curl_setopt($ch, CURLOPT_URL, $url . '?' . http_build_query($args));
+            } else {
+                curl_setopt($ch, CURLOPT_URL, $url);
+            }
+        }
 
-    	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    	if ($this->getVerifyPeerSSL() === false)
-    	{
-    		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    	}
-    	// Make sure we can access the response when we execute the call
-    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        if ($this->getVerifyPeerSSL() === false) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        }
+        // Make sure we can access the response when we execute the call
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    	$data = curl_exec($ch);
+        $data = curl_exec($ch);
 
-    	if ($data === false) 
-    	{
-    		return array('error' => 'API call failed with cURL error: ' . curl_error($ch));
-    	}
+        if ($data === false) {
+            return array('error' => 'API call failed with cURL error: ' . curl_error($ch));
+        }
 
-    	$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
  
-    	curl_close($ch);
+        curl_close($ch);
 
-    	$response = json_decode($data, true);
+        $response = json_decode($data, true);
 
-	    $jsonErrorCode = json_last_error();
-	    if ($jsonErrorCode !== JSON_ERROR_NONE) 
-	    {
-	    	return array(
-	    		'error' => 'API response not well-formed (json error code: ' . $jsonErrorCode . ')');
-	    }
+        $jsonErrorCode = json_last_error();
+        if ($jsonErrorCode !== JSON_ERROR_NONE) {
+            return array(
+                'error' => 'API response not well-formed (json error code: '
+                    . $jsonErrorCode . ')'
+            );
+        }
 
-    	if ($httpCode !== $successCode) 
-    	{
-	        $errorMessage = '';
+        if ($httpCode !== $successCode) {
+            $errorMessage = '';
 
-	        if (!empty($response['error_description'])) 
-	        {
-	            $errorMessage = $response['error_description'];
-	        }
-	        elseif (!empty($response['error'])) 
-	        {
-	            $errorMessage = $response['error'];
-	        }
-	        elseif (!empty($response['description'])) 
-	        {
-	            $errorMessage = $response['description'];
-	        }
-	        elseif (!empty($response['message'])) 
-	        {
-	            $errorMessage = $response['message'];
-	        }
-	        elseif (!empty($response['requestError']['serviceException'])) 
-	        {
-	            $errorMessage = $response['requestError']['serviceException']['text'] .
-	                ' ' . $response['requestError']['serviceException']['variables'];
-	        }
-	        elseif (!empty($response['requestError']['policyException'])) 
-	        {
-	            $errorMessage = $response['requestError']['policyException']['text'] .
-	                ' ' . $response['requestError']['policyException']['variables'];
-	        }
+            if (!empty($response['error_description'])) {
+                $errorMessage = $response['error_description'];
+            } elseif (!empty($response['error'])) {
+                $errorMessage = $response['error'];
+            } elseif (!empty($response['description'])) {
+                $errorMessage = $response['description'];
+            } elseif (!empty($response['message'])) {
+                $errorMessage = $response['message'];
+            } elseif (!empty($response['requestError']['serviceException'])) {
+                $errorMessage = $response['requestError']['serviceException']['text']
+                    . ' ' . $response['requestError']['serviceException']['variables'];
+            } elseif (!empty($response['requestError']['policyException'])) {
+                $errorMessage = $response['requestError']['policyException']['text']
+                    . ' ' . $response['requestError']['policyException']['variables'];
+            }
 
-	        return array('error' => $errorMessage);
-    	}
+            return array('error' => $errorMessage);
+        }
 
-    	return $response;
+        return $response;
     }
 
     /**
-	 *  Get the Client Secret.
-	 *
-	 * @return string
-	 */
+     *  Get the Client Secret.
+     *
+     * @return string
+     */
     public function getClientSecret()
     {
-    	return $this->clientSecret;
+        return $this->clientSecret;
     }
 
     /**
-	 *  Get the Token.
-	 *
-	 * @return string
-	 */
+     *  Get the Token.
+     *
+     * @return string
+     */
     public function getToken()
     {
-    	return $this->token;
+        return $this->token;
     }
 
     /**
-	 *  Set the Token value.
-	 *
-	 * @return string
-	 */
+     *  Set the Token value.
+     *
+     * @return string
+     */
     public function setToken($token)
     {
-    	$this->token = $token;
+        $this->token = $token;
     }
 
     /**
-	 *  Get CURLOPT_SSL_VERIFYPEER value.
-	 *
-	 * @return bool
-	 */
+     *  Get CURLOPT_SSL_VERIFYPEER value.
+     *
+     * @return bool
+     */
     public function getVerifyPeerSSL()
     {
-    	return $this->verifyPeerSSL;
+        return $this->verifyPeerSSL;
     }
 }
